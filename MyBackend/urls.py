165c0b06bb1b3dff655e0e1485239d899cac40f9
Geromin-dot <grv_manese.student@ua.edu.ph>
2025-11-19 09:37:55 +0800

    Added Templates and More

--- MyBackend/urls.py
+++ MyBackend/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.homepage_view , name='homepage_html'),
+    path('login/', views.login_view , name='login_html'),
     path('admin/', admin.site.urls),
     path('registration/', include('registration.urls')), 
+
+
 ]
